fb42c4ca832dfb4b7ba894f507cdfbfb79ec0ac8
Nicolas Daniel Suarez Reyes <nicosua.reyes15@gmail.com>
2024-09-15 16:50:02 -0500

    Update Adivina el numero.py

--- Adivina el numero.py
+++ Adivina el numero.py
@@ -2,7 +2,7 @@ import random
 def adivinar_numero():
     numero_secreto = random.randint(1, 10)
     adivinado = False
-    print("He generado un número entre 1 y 100. Intenta adivinarlo.")
+    print("He generado un número entre 1 y 10. Intenta adivinarlo.")
     while not adivinado:
         try:
             intento = int(input("Ingresa tu intento: "))
